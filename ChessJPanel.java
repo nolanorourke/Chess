5cb5a4a6f1a96ad8f656e61fcf57601d71e3441d
Nolan ORourke <ngorourke@gmail.com>
2023-12-08 03:31:00 -0500

    added some buttons and what not

--- ChessJPanel.java
+++ ChessJPanel.java
@@ -8,8 +8,12 @@ import java.awt.Graphics;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.GridLayout;
+import java.awt.Image;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.awt.event.MouseListener;
+import java.awt.event.MouseEvent;
+
 
 import javax.swing.JLabel;
 import javax.swing.BoxLayout;
@@ -17,7 +21,14 @@ import javax.swing.JButton;
 import javax.swing.JPanel;
 import javax.swing.JTable;
 import javax.swing.table.DefaultTableModel;
+
 import javax.swing.JScrollPane;
+import javax.swing.Icon;
+import javax.swing.ImageIcon;
+import javax.swing.SwingConstants;
+import javax.swing.border.LineBorder;
+
+import java.net.URL;
 
 import Chess_Board.GameBoard;
 import Chess_Board.Spot;
@@ -30,6 +41,7 @@ public class ChessJPanel extends JPanel {
     private GameBoard currentBoard;
     private CenterPanel centerPanel;
     private EastPanel eastPanel;
+    private BottomPanel bottomPanel;
 
     ChessJPanel() {
         // Sets JPanel
@@ -38,9 +50,10 @@ public class ChessJPanel extends JPanel {
         // Adds panels
         centerPanel = new CenterPanel();
         eastPanel = new EastPanel();
+        bottomPanel = new BottomPanel();
         add(centerPanel, BorderLayout.CENTER);
         add(eastPanel, BorderLayout.EAST);
-        add(new BottomPanel(), BorderLayout.SOUTH);
+        add(bottomPanel, BorderLayout.SOUTH);
     }
 
     // Used to dynamically get width, height, and make sure Panel is refereshed
@@ -72,7 +85,7 @@ public class ChessJPanel extends JPanel {
 
             if (currentString != currentBoard.getMoveInterp()) {
                 currentString = currentBoard.getMoveInterp();
-                eastPanel.addMoveToTable(currentString, teamnum);
+                eastPanel.addMoveToTable(currentString, teamnum, eastPanel.getTurnNum());
                 if (teamnum == 1)
                     ++teamnum;
                 else
@@ -92,21 +105,10 @@ public class ChessJPanel extends JPanel {
             currentBoard = new GameBoard();
             add(new TopPanel(), BorderLayout.NORTH);
             add(currentBoard);
-            eastPanel = new EastPanel();
-
         }
 
         public void paintComponent(Graphics g) {
             super.paintComponent(g);
-        }
-
-        // Resets the chess board
-        public void resetBoard() {
-            remove(currentBoard);
-            currentBoard = new GameBoard();
-            add(currentBoard);
-            eastPanel.clearMoveHistory();
-            update(getGraphics());
         }
 
     }
@@ -119,8 +121,10 @@ public class ChessJPanel extends JPanel {
         private DefaultTableModel moveHistory;
         private JTable moveHistoryTable;
         private JScrollPane scroller;
+        private int turnNum;
 
         EastPanel() {
+            turnNum = 1;
             moveHistory = new DefaultTableModel() {
                 // overwriting the isCellEditable function to always be false
                 public boolean isCellEditable(int row, int column) {
@@ -155,10 +159,10 @@ public class ChessJPanel extends JPanel {
                     moveHistoryTable.getColumnModel().getColumn(2).getWidth(), getHeight()));
         }
 
-        public void addMoveToTable(String move, int teamnum) {
+        public void addMoveToTable(String move, int teamnum, int turn) {
             if (teamnum == 1) {
                 // since team one goes first, it will create a new row
-                moveHistory.addRow(new Object[] { moveHistory.getRowCount() + 1, null, null });
+                moveHistory.addRow(new Object[] { turn, null, null });
                 moveHistory.setValueAt(move, moveHistory.getRowCount() - 1, 1);
             } else if (teamnum == 2) {
 
@@ -175,46 +179,196 @@ public class ChessJPanel extends JPanel {
         public void clearMoveHistory() {
             moveHistory.getDataVector().removeAllElements();
         }
+        public int getTurnNum()
+        {
+            return turnNum;
+        }
     }
 
     public class BottomPanel extends JPanel 
     {
-        private JButton newGame, reset;
+        private JButton newGame, reset, instructions;
+        private JLabel newGameLabel, resetLabel, howToPlayLabel;
+        private Icon newGame1, newGame2, reset1, reset2, howToPlay1, howToPlay2;
 
-        BottomPanel() {
-            setLayout(new FlowLayout(FlowLayout.RIGHT, 10, 5));
+        BottomPanel() 
+        {
+            setLayout(new FlowLayout(FlowLayout.RIGHT, 10, getWidth()/3));
+            // newGame1 = new ImageIcon(getClass().getResource("NewGame1.png"));
+            // newGame2 = new ImageIcon(getClass().getResource("NewGame2.png"));
 
-            // Resets game button
-            newGame = new JButton("New Game");
-            newGame.addActionListener(new ActionListener() {
-                public void actionPerformed(ActionEvent e) {
-                    centerPanel.resetBoard();
-                }
-            });
-            reset = new JButton("Reset");
+            // reset1 = new ImageIcon(getClass().getResource("Reset1.png"));
 
-            // Takes off the border around text when you click button
-            newGame.setFocusPainted(false);
-            reset.setFocusPainted(false);
+            // howToPlay1 = new ImageIcon(getClass().getResource("HowToPlay1.png"));
+            // howToPlay2 = new ImageIcon(getClass().getResource("HowToPlay2.png"));  
 
-            newGame.setPreferredSize(new Dimension(95, 26));
+            //resizes images using function, stay tuned
+            newGame1 = resizeImageIcon(new ImageIcon(getClass().getResource("NewGame1.png")));
+            reset1 = resizeImageIcon(new ImageIcon(getClass().getResource("Reset1.png")));
+            reset2 = resizeImageIcon(new ImageIcon(getClass().getResource("Reset2.png")));
+            howToPlay1 = resizeImageIcon(new ImageIcon(getClass().getResource("HowToPlay1.png")));
+            newGame2 = resizeImageIcon(new ImageIcon(getClass().getResource("NewGame2.png")));
+            howToPlay2 = resizeImageIcon(new ImageIcon(getClass().getResource("HowToPlay2.png")));
 
-            add(newGame);
-            add(reset);
+            newGameLabel = new JLabel("New Game", newGame1, SwingConstants.RIGHT);
+            newGameLabel.setToolTipText("Start a new game");
+            newGameLabel.setVerticalTextPosition( SwingConstants.BOTTOM );
+            newGameLabel.setHorizontalTextPosition( SwingConstants.CENTER );
+
+
+            resetLabel = new JLabel("Reset", reset1, SwingConstants.LEFT);
+            resetLabel.setToolTipText("Reset current game to start");
+            resetLabel.setVerticalTextPosition( SwingConstants.BOTTOM );
+            resetLabel.setHorizontalTextPosition( SwingConstants.CENTER );
+
+
+            howToPlayLabel = new JLabel("How To Play", howToPlay1, SwingConstants.CENTER);
+            howToPlayLabel.setToolTipText("Open page to Instructions");
+            howToPlayLabel.setVerticalTextPosition( SwingConstants.BOTTOM );
+            howToPlayLabel.setHorizontalTextPosition( SwingConstants.CENTER );
+
+            MouseHandler mousey = new MouseHandler();
+            newGameLabel.addMouseListener(mousey);
+            resetLabel.addMouseListener(mousey);
+            howToPlayLabel.addMouseListener(mousey);
+
+            add(newGameLabel);
+            add(resetLabel);
+            add(howToPlayLabel);
+//15_16
+            //Resets game button
+            // newGame = new JButton("New Game", newGame1);
+            // newGame.addActionListener(new ActionListener()
+            // {
+            //     public void actionPerformed(ActionEvent e)
+            //     {
+
+            //     }
+            // });
+            // reset = new JButton("Reset", reset1);
+            // reset.addActionListener(new ActionListener() 
+            // {
+            //     public void actionPerformed(ActionEvent e) 
+            //     {
+                    
+            //     }
+            // });
+            // //instructions = new JButton("How To Play", howToPlay1);
+
+
+
+            // //Takes off the border around text when you click button
+            // newGame.setFocusPainted(false);
+            // reset.setFocusPainted(false);
+
+            // newGame.setPreferredSize(new Dimension(95, 26));
+
+            // add(newGame);
+            // add(reset);
+
+        }
+        private class MouseHandler implements MouseListener {
+        public void mouseExited(MouseEvent e) {
+            if(e.getSource() ==newGameLabel)
+            {
+                newGameLabel.setIcon(newGame1);
+            }
+            else if(e.getSource() == resetLabel)
+            {
+                resetLabel.setIcon(reset1);
+            }
+            else if(e.getSource() == howToPlayLabel)
+            {
+                howToPlayLabel.setIcon(howToPlay1);
+            }
+
+         }
+
+        public void mouseEntered(MouseEvent e) {
+            if(e.getSource() ==newGameLabel)
+            {
+                newGameLabel.setIcon(newGame2);
+            }
+            else if(e.getSource() == resetLabel)
+            {
+                resetLabel.setIcon(reset2);
+            }
+            else if(e.getSource() == howToPlayLabel)
+            {
+                howToPlayLabel.setIcon(howToPlay2);
+            }
+
+         }
+
+        public void mouseClicked(MouseEvent e) {
+            if(e.getSource() ==newGameLabel)
+            {
+                newGameLabel.setIcon(newGame2);
+                newGameLabel.setIcon(newGame1);
+                startNewGame();
+            }
+            else if(e.getSource() == resetLabel)
+            {
+                resetBoard();
+                resetLabel.setIcon(reset2);
+                resetLabel.setIcon(reset1);
+            }
+            else if(e.getSource() == howToPlayLabel)
+            {
+                howToPlayLabel.setIcon(howToPlay2);
+                howToPlayLabel.setIcon(howToPlay1);
+            }
 
         }
 
+        public void mousePressed(MouseEvent e) {
+        }
+
+        public void mouseReleased(MouseEvent e){ 
+        }
+    }
+
+    private ImageIcon resizeImageIcon(ImageIcon icon)
+    {
+        Image image = icon.getImage();
+        Image scaledImage = image.getScaledInstance(50, 50, Image.SCALE_SMOOTH); // Adjust the size as needed
+        return new ImageIcon(scaledImage);
+    }
         // Dynamically resize
         public void paintComponent(Graphics g) {
             // Resizes Frame
             super.paintComponent(g);
-            setPreferredSize(new Dimension(getPreferredSize().width, (int) (height * 0.1)));
+            //setPreferredSize(new Dimension(getPreferredSize().width, (int) (height * 0.1)));
+
+
+
 
             // Resizes button and font
-            newGame.setPreferredSize(new Dimension((int) (0.15 * width), (int) (0.06 * height)));
-            newGame.setFont(new Font(newGame.getFont().getName(), newGame.getFont().getStyle(), (int) ((height + width) * 0.01)));
-            reset.setPreferredSize(new Dimension((int) (0.12 * width), (int) (0.06 * height)));
-            reset.setFont(new Font(reset.getFont().getName(), reset.getFont().getStyle(), (int) ((height + width) * 0.01)));
+            // newGameLabel.setPreferredSize(new Dimension((int) (0.15 * width), (int) (0.06 * height)));
+            // newGameLabel.setFont(new Font(newGame.getFont().getName(), newGame.getFont().getStyle(), (int) ((height + width) * 0.01)));
+            // resetLabel.setPreferredSize(new Dimension((int) (0.12 * width), (int) (0.06 * height)));
+            //resetLabel.setFont(new Font(reset.getFont().getName(), reset.getFont().getStyle(), (int) ((height + width) * 0.01)));
         }
+    }
+
+    public void resetBoard()
+    {
+        remove(currentBoard);
+        currentBoard = new GameBoard();
+        add(currentBoard);
+        eastPanel.turnNum = 1;
+        update(getGraphics());
+        
+    }
+    public void startNewGame()
+    {
+        remove(currentBoard);
+        remove(eastPanel);
+        currentBoard = new GameBoard();
+        eastPanel = new EastPanel();
+        add(currentBoard);
+        add(eastPanel);
+        eastPanel.turnNum = 1;
+        update(getGraphics());
     }
 }
